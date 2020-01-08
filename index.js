f4ef4441d9a63f9d233bd5857edf46945ae364e3
TysonVonnBeck <54685544+tysonvonnbeck@users.noreply.github.com>
2020-01-08 10:49:11 -0800

    update

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 var express = require('express')
 var app = express();
 
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req,res) => {
+    res.send('Welcome to my API');
+})
+
+app.get('/coffee', (req,res)=>{
+    res.json({
+        name:"coffee",
+        bestFood:true
+    })
+})
+
+app.listen(PORT, function() {
+    console.log('listening on port ' + PORT);
+}
+)
